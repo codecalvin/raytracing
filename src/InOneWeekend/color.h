0e7d14dab6a68fb64d6346da0c38fd8ff5799823
coder <calvin@gritworld.com>
2025-01-09 22:54:56 +0800

    message update

--- src/InOneWeekend/color.h
+++ src/InOneWeekend/color.h
@@ -19,31 +19,37 @@ using color = vec3;
 
 inline double linear_to_gamma(double linear_component)
 {
-    if (linear_component > 0)
-        return std::sqrt(linear_component);
+	if (linear_component > 0)
+		return std::sqrt(linear_component);
 
-    return 0;
+	return 0;
+}
+
+inline color linear_to_gamma(const color& pixel_color)
+{
+	return {
+		linear_to_gamma(pixel_color[0]),
+linear_to_gamma(pixel_color[1]),
+linear_to_gamma(pixel_color[2])
+	};
 }
 
 
+inline void convert_color(const color& pixel_color, uint8_t* output) {
+	auto gamma_color = linear_to_gamma(pixel_color);
+
+	// Translate the [0,1] component values to the byte range [0,255].
+	static const interval intensity(0.000, 0.999);
+	output[0] = uint8_t(256 * intensity.clamp(gamma_color.x()));
+	output[1] = uint8_t(256 * intensity.clamp(gamma_color.y()));
+	output[2] = uint8_t(256 * intensity.clamp(gamma_color.z()));
+}
+
 void write_color(std::ostream& out, const color& pixel_color) {
-    auto r = pixel_color.x();
-    auto g = pixel_color.y();
-    auto b = pixel_color.z();
-
-    // Apply a linear to gamma transform for gamma 2
-    r = linear_to_gamma(r);
-    g = linear_to_gamma(g);
-    b = linear_to_gamma(b);
-
-    // Translate the [0,1] component values to the byte range [0,255].
-    static const interval intensity(0.000, 0.999);
-    int rbyte = int(256 * intensity.clamp(r));
-    int gbyte = int(256 * intensity.clamp(g));
-    int bbyte = int(256 * intensity.clamp(b));
-
-    // Write out the pixel color components.
-    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+	// Write out the pixel color components.
+	uint8_t rgb[3];
+	convert_color(pixel_color, rgb);
+	out << rgb[0] << ' ' << rgb[1] << ' ' << rgb[2] << '\n';
 }
 
 
--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -16,89 +16,96 @@
 #include "hittable_list.h"
 #include "material.h"
 #include "sphere.h"
-#include <fstream>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 
 void camera::render(const hittable& world)
 {
-    initialize();
+	initialize();
 
-    std::ofstream stream("image.ppm", std::ios_base::binary);
-    stream << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+	constexpr int image_channel = 3;
+	std::unique_ptr<uint8_t[]> image_data = std::make_unique<uint8_t[]>(image_width * image_height * image_channel);
 
-    for (int j = 0; j < image_height; j++) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; i++) {
-            color pixel_color(0, 0, 0);
-            for (int sample = 0; sample < samples_per_pixel; sample++) {
-                ray r = get_ray(i, j);
-                pixel_color += ray_color(r, max_depth, world);
-            }
-            write_color(stream, pixel_samples_scale * pixel_color);
-        }
-    }
+	auto image_ptr = image_data.get();
+	for (int j = 0; j < image_height; j++) {
+		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+		for (int i = 0; i < image_width; i++) {
+			color pixel_color(0, 0, 0);
+			for (int sample = 0; sample < samples_per_pixel; sample++) {
+				ray r = get_ray(i, j);
+				pixel_color += ray_color(r, max_depth, world);
+			}
+			convert_color(pixel_samples_scale * pixel_color, image_ptr);
+			image_ptr += 3;
+		}
+	}
 
-    std::clog << "\rDone.                 \n";
+	stbi_write_png("image.png", image_width, image_height, image_channel, image_data.get(), image_width * image_channel);
+	std::clog << "\rDone.                 \n";
 }
 
 
 
 int main() {
-    hittable_list world;
+	hittable_list world;
 
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
-            auto choose_mat = random_double();
-            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			auto choose_mat = random_double();
+			point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
 
-            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-                shared_ptr<material> sphere_material;
+			if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+				shared_ptr<material> sphere_material;
 
-                if (choose_mat < 0.8) {
-                    // diffuse
-                    auto albedo = color::random() * color::random();
-                    sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                } else if (choose_mat < 0.95) {
-                    // metal
-                    auto albedo = color::random(0.5, 1);
-                    auto fuzz = random_double(0, 0.5);
-                    sphere_material = make_shared<metal>(albedo, fuzz);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                } else {
-                    // glass
-                    sphere_material = make_shared<dielectric>(1.5);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                }
-            }
-        }
-    }
+				if (choose_mat < 0.8) {
+					// diffuse
+					auto albedo = color::random() * color::random();
+					sphere_material = make_shared<lambertian>(albedo);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+				else if (choose_mat < 0.95) {
+					// metal
+					auto albedo = color::random(0.5, 1);
+					auto fuzz = random_double(0, 0.5);
+					sphere_material = make_shared<metal>(albedo, fuzz);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+				else {
+					// glass
+					sphere_material = make_shared<dielectric>(1.5);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+			}
+		}
+	}
 
-    auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+	auto material1 = make_shared<dielectric>(1.5);
+	world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
 
-    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+	auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+	world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
 
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+	auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+	world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
-    camera cam;
+	camera cam;
 
-    cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 1200;
-    cam.samples_per_pixel = 10;
-    cam.max_depth         = 20;
+	cam.aspect_ratio = 16.0 / 9.0;
+	cam.image_width = 1200;
+	cam.samples_per_pixel = 10;
+	cam.max_depth = 20;
 
-    cam.vfov     = 20;
-    cam.lookfrom = point3(13,2,3);
-    cam.lookat   = point3(0,0,0);
-    cam.vup      = vec3(0,1,0);
+	cam.vfov = 20;
+	cam.lookfrom = point3(13, 2, 3);
+	cam.lookat = point3(0, 0, 0);
+	cam.vup = vec3(0, 1, 0);
 
-    cam.defocus_angle = 0.6;
-    cam.focus_dist    = 10.0;
+	cam.defocus_angle = 0.6;
+	cam.focus_dist = 10.0;
 
-    cam.render(world);
+	cam.render(world);
 }
